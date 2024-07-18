279715947fbe2be6fe47c12720aaf1f4eacb45f5
ousmane <odrame68@gmail.com>
2024-07-18 13:16:53 +0000

    add readme file

---
+++ readme.md
@@ -0,0 +1,5 @@
+### configuration de spring security
+# creer en premier le fichier UserDetailsServiceImpl qui implemente la class OncePerRequestFilter
+# puis la class jwtFilter et apres la classe BeanConfig pour gerer le AuthProvider
+# et en fin la class SecurityConfig
+v
--- src/main/java/com/ousmane/bookweb/security/JwtFilter.java
+++ src/main/java/com/ousmane/bookweb/security/JwtFilter.java
@@ -14,8 +14,7 @@ public class JwtFilter extends OncePerRequestFilter{
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+        
     }
 
 }
